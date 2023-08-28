8e2074879b4c091efc9b1ae9d834838498483243
多啦b梦 <1715109585@qq.com>
2023-08-28 22:55:43 +0800

    ok

--- wails-demo/frontend/src/stores/窗口事件.js
+++ wails-demo/frontend/src/stores/窗口事件.js
@@ -13,6 +13,10 @@ export function 绑定窗口事件(窗口) {
     窗口.按钮2_被单击 = function () {
         console.log("按钮2_被单击")
     }
+
+    窗口.按钮2_鼠标左键被按下 = function () {
+        console.log("按钮2_鼠标左键被按下")
+    }
 //事件函数位置不要删除
 }
 
