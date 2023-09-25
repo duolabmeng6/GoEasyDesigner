a04a51303cb7a89c5788eb1a4c32c158f39496c0
多啦b梦 <1715109585@qq.com>
2023-09-25 12:47:56 +0800

    进度条组件封装

---
+++ GoEasyDesigner/frontend/src/components/组件/进度条.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-slider
+        :style="item.尺寸=='custom' ? getItemStyle(item):''"
+        style="width: 80%;margin:0px 20px"
+        v-model="item.位置"
+               :size="item.尺寸=='custom' ?'':item.尺寸"
+               :min="item.最小值"
+               :max="item.最大值"
+               :step="item.步长"
+    />
+
+</template>
+
+<script>
+import {getItemStyle} from "@/public";
+
+export default {
+  methods: {getItemStyle},
+  props: {
+    item: {
+      type: Object,
+      default: '',
+    }
+  },
+}
+</script>
