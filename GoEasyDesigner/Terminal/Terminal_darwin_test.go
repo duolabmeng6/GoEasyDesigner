5381de415990298824d89f3f800accd4e9ccf330
多啦b梦 <1715109585@qq.com>
2023-09-26 18:30:21 +0800

    测试ctrl c 结束

---
+++ GoEasyDesigner/Terminal/Terminal_darwin_test.go
@@ -0,0 +1,33 @@
+package Terminal
+
+import (
+	"testing"
+	"time"
+)
+
+func TestNewTerminalMac(t *testing.T) {
+	terminal := NewTerminal()
+	if terminal == nil {
+		t.Error("NewTerminalWindow() failed")
+	}
+	//
+	命令 := `
+cd /Users/ll/Documents/GitHub/GoEasyDesigner/wails-demo/frontend
+npm run dev
+`
+
+	terminal.StartCommand(命令, func(output string, err error) {
+		if err != nil {
+			t.Error(err.Error())
+		}
+		println(output)
+	})
+
+	time.Sleep(3 * time.Second)
+	terminal.StopCommand2()
+	println("停止命令2")
+	time.Sleep(10 * time.Second)
+	terminal.StopCommand()
+	println("停止命令1")
+
+}
