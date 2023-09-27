c0b38876ead455c46c42f2719c5639ec10ca8764
多啦b梦 <1715109585@qq.com>
2023-09-27 11:32:11 +0800

    Update vite.config.js

--- wails-demo/frontend/vite.config.js
+++ wails-demo/frontend/vite.config.js
@@ -1,24 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    optimizeDeps: {
+        include: [
+            './src/窗口/设计文件.json',
+            './src/窗口/窗口事件.js',
+            './src/窗口/辅助代码.js',
+            './src/窗口/窗口事件.js',
+        ]
     }
-  },
-  optimizeDeps: {
-    include: [
-      './src/窗口/组件数据.json',
-        './src/窗口/窗口事件.js',
-        './src/窗口/辅助代码.js',
-        './src/窗口/窗口事件.js',
-    ]
-  }
 })
