6720a941bfe61687b585282aa027ab22c18ef681
多啦b梦 <1715109585@qq.com>
2023-10-13 02:10:05 +0800

    编译 测试

--- wails-demo/frontend/src/窗口/窗口事件.js
+++ wails-demo/frontend/src/窗口/窗口事件.js
@@ -1,22 +1,8 @@
 export function 绑定窗口事件(c, 组件) {
 
-
-    c.按钮1被单击 = function () {
-        console.log("按钮1被单击")
-        组件.编辑框1.内容="祖国你好!"
-    }
-
-    c.编辑框1被单击 = function () {
-        console.log("编辑框1被单击")
-    }
-
     c.按钮2被单击 = function () {
         console.log("按钮2被单击")
-    }
-
-    c.按钮3被单击 = function () {
-        console.log("按钮3被单击")
+        组件.编辑框1.内容 = "祖国你好!"
     }
 //事件函数位置不要删除
 }
-
