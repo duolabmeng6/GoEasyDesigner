3454d17fe0b99af704c1313fdec84339fdb38e90
多啦b梦 <1715109585@qq.com>
2023-10-22 14:07:39 +0800

    ok

--- GoEasyDesigner/frontend/src/update_github_latest_releases.sh
+++ GoEasyDesigner/frontend/src/update_github_latest_releases.sh
@@ -12,30 +12,5 @@
 JSON_FILE="releases_latest.json"
 URL="https://api.github.com/repos/duolabmeng6/GoEasyDesigner/releases"
 
-# 检查文件是否存在并且修改时间是否超过1天
-if [ -e "$JSON_FILE" ]; then
-    # 获取文件的最后修改时间
-    LAST_MODIFIED=""
-    if command -v stat &> /dev/null; then
-        # macOS
-        LAST_MODIFIED=$(date -r "$JSON_FILE" +%s)
-    elif command -v stat &> /dev/null; then
-        # Linux
-        LAST_MODIFIED=$(date -d "$(stat -c %y "$JSON_FILE")" +%s)
-    fi
-
-    CURRENT_TIME=$(date +%s)
-    TIME_DIFF=$((CURRENT_TIME - LAST_MODIFIED))
-
-    if [ $TIME_DIFF -gt 86400 ]; then
-        # 文件超过1天，重新下载
-        echo "文件超过1天未更新，重新下载..."
-        curl -o "$JSON_FILE" "$URL"
-    else
-        echo "文件未超过1天，无需重新下载 经过多少秒后检查" $TIME_DIFF
-    fi
-else
-    # 文件不存在，直接下载
-    echo "文件不存在，直接下载..."
-    curl -o "$JSON_FILE" "$URL"
-fi
+echo "正在下载最新的releases..."
+curl -o "$JSON_FILE" "$URL"
