f6dd3aa634bbc619193c54b98d39cf1604521274
多啦b梦 <1715109585@qq.com>
2023-11-04 02:51:46 +0800

    ok

--- GoEasyDesigner/frontend/src/编辑器/窗口事件代码模板.js
+++ GoEasyDesigner/frontend/src/编辑器/窗口事件代码模板.js
@@ -1,3 +1,6 @@
-export const 窗口事件代码模板 =`export function BindWindowEvent(c, comps) {
+export const 窗口事件代码模板 =`import * as SystemFc from "../../wailsjs/runtime";
+import * as goFc from "../../wailsjs/go/main/App";
+
+export function BindWindowEvent(c, comps) {
 //Don't delete the event function flag
 }`
